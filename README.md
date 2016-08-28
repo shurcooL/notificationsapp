13459c17595dbe97bdc6f9eb16c480787cf3bed8
Dmitri Shuralyov <shurcooL@gmail.com>
2016-08-27 23:14:42 -0700

    Link to canonical issue tracker more visibly.

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 Package notificationsapp is a web frontend for a notifications service.
 
+Note, the canonical issue tracker for this package is currently hosted at
+https://dmitri.shuralyov.com/issues/github.com/shurcooL/notificationsapp.
+Its notifications are implemented using this very package.
+
 Installation
 ------------
 
--- doc.go
+++ doc.go
@@ -1,4 +1,8 @@
 //go:generate vfsgendev -source="github.com/shurcooL/notificationsapp".Assets
 
 // Package notificationsapp is a web frontend for a notifications service.
+//
+// Note, the canonical issue tracker for this package is currently hosted at
+// https://dmitri.shuralyov.com/issues/github.com/shurcooL/notificationsapp.
+// Its notifications are implemented using this very package.
 package notificationsapp
