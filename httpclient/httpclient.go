eaaea8af7dd0b3692204814f59dbee767dd7499e
Dmitri Shuralyov <shurcooL@gmail.com>
2017-05-02 19:19:21 -0400

    httpclient: Add HTTP client, base URL parameters.
    It's now possible for the client to connect to a remote server, and use
    a custom HTTP client with authentication.

--- httpclient/httpclient.go
+++ httpclient/httpclient.go
@@ -15,10 +15,26 @@ import (
 	"github.com/shurcooL/users"
 )
 
-// Notifications implements notifications.Service remotely over HTTP.
-type Notifications struct{}
+// NewNotifications creates a client that implements notifications.Service remotely over HTTP.
+// If a nil httpClient is provided, http.DefaultClient will be used.
+// scheme and host can be empty strings to target local service.
+func NewNotifications(httpClient *http.Client, scheme, host string) notifications.Service {
+	return &notificationsClient{
+		client: httpClient,
+		baseURL: &url.URL{
+			Scheme: scheme,
+			Host:   host,
+		},
+	}
+}
 
-func (Notifications) List(ctx context.Context, opt notifications.ListOptions) (notifications.Notifications, error) {
+// notificationsClient implements notifications.Service remotely over HTTP.
+type notificationsClient struct {
+	client  *http.Client // HTTP client for API requests. If nil, http.DefaultClient should be used.
+	baseURL *url.URL     // Base URL for API requests.
+}
+
+func (n *notificationsClient) List(ctx context.Context, opt notifications.ListOptions) (notifications.Notifications, error) {
 	v := url.Values{} // TODO: Automate this conversion process.
 	if opt.Repo != nil {
 		v.Set("RepoURI", opt.Repo.URI)
@@ -27,7 +43,7 @@ func (Notifications) List(ctx context.Context, opt notifications.ListOptions) (n
 		Path:     httproute.List,
 		RawQuery: v.Encode(),
 	}
-	resp, err := ctxhttp.Get(ctx, nil, u.String())
+	resp, err := ctxhttp.Get(ctx, n.client, n.baseURL.ResolveReference(&u).String())
 	if err != nil {
 		return nil, err
 	}
@@ -41,8 +57,8 @@ func (Notifications) List(ctx context.Context, opt notifications.ListOptions) (n
 	return ns, err
 }
 
-func (Notifications) Count(ctx context.Context, opt interface{}) (uint64, error) {
-	resp, err := ctxhttp.Get(ctx, nil, httproute.Count)
+func (n *notificationsClient) Count(ctx context.Context, opt interface{}) (uint64, error) {
+	resp, err := ctxhttp.Get(ctx, n.client, n.baseURL.ResolveReference(&url.URL{Path: httproute.Count}).String())
 	if err != nil {
 		return 0, err
 	}
@@ -56,7 +72,7 @@ func (Notifications) Count(ctx context.Context, opt interface{}) (uint64, error)
 	return u, err
 }
 
-func (Notifications) MarkRead(ctx context.Context, appID string, repo notifications.RepoSpec, threadID uint64) error {
+func (n *notificationsClient) MarkRead(ctx context.Context, appID string, repo notifications.RepoSpec, threadID uint64) error {
 	u := url.URL{
 		Path: httproute.MarkRead,
 		RawQuery: url.Values{
@@ -65,7 +81,7 @@ func (Notifications) MarkRead(ctx context.Context, appID string, repo notificati
 			"ThreadID": {fmt.Sprint(threadID)},
 		}.Encode(),
 	}
-	resp, err := ctxhttp.Post(ctx, nil, u.String(), "", nil)
+	resp, err := ctxhttp.Post(ctx, n.client, n.baseURL.ResolveReference(&u).String(), "", nil)
 	if err != nil {
 		return err
 	}
@@ -77,14 +93,14 @@ func (Notifications) MarkRead(ctx context.Context, appID string, repo notificati
 	return nil
 }
 
-func (Notifications) MarkAllRead(ctx context.Context, repo notifications.RepoSpec) error {
+func (n *notificationsClient) MarkAllRead(ctx context.Context, repo notifications.RepoSpec) error {
 	u := url.URL{
 		Path: httproute.MarkAllRead,
 		RawQuery: url.Values{
 			"RepoURI": {repo.URI},
 		}.Encode(),
 	}
-	resp, err := ctxhttp.Post(ctx, nil, u.String(), "", nil)
+	resp, err := ctxhttp.Post(ctx, n.client, n.baseURL.ResolveReference(&u).String(), "", nil)
 	if err != nil {
 		return err
 	}
@@ -96,10 +112,10 @@ func (Notifications) MarkAllRead(ctx context.Context, repo notifications.RepoSpe
 	return nil
 }
 
-func (Notifications) Subscribe(_ context.Context, appID string, repo notifications.RepoSpec, threadID uint64, subscribers []users.UserSpec) error {
+func (notificationsClient) Subscribe(_ context.Context, appID string, repo notifications.RepoSpec, threadID uint64, subscribers []users.UserSpec) error {
 	return fmt.Errorf("Subscribe: not implemented")
 }
 
-func (Notifications) Notify(_ context.Context, appID string, repo notifications.RepoSpec, threadID uint64, nr notifications.NotificationRequest) error {
+func (notificationsClient) Notify(_ context.Context, appID string, repo notifications.RepoSpec, threadID uint64, nr notifications.NotificationRequest) error {
 	return fmt.Errorf("Notify: not implemented")
 }
--- httpclient/httpclient_test.go
+++ httpclient/httpclient_test.go
@@ -1,8 +1,24 @@
 package httpclient_test
 
 import (
-	"github.com/shurcooL/notifications"
+	"context"
+
 	"github.com/shurcooL/notificationsapp/httpclient"
+	"golang.org/x/oauth2"
 )
 
-var _ notifications.Service = httpclient.Notifications{}
+func ExampleNewNotifications() {
+	// HTTP client with authentication.
+	src := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: "... your access token ..."},
+	)
+	httpClient := oauth2.NewClient(context.Background(), src)
+
+	notificationsClient := httpclient.NewNotifications(httpClient, "http", "localhost:8080")
+
+	// Now you can use any of notificationsClient methods.
+
+	// Output:
+
+	_ = notificationsClient
+}
